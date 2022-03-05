9c8011ea6f9d2996bd871da7cd5a3c97158602e8
DavidLS <DavidLS@LAPTOP-F803EC2K>
2022-03-04 18:27:07 -0600

    init commit

--- src/pruebagithub/PruebaGitHub.java
+++ src/pruebagithub/PruebaGitHub.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package pruebagithub;
+import java.util.Scanner;
 
 /**
  *
@@ -13,8 +14,30 @@ public class PruebaGitHub {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
     
-}
+    public static void main(String[] args) {
+    
+        System.out.println("Introduce una palabra y te diré si es palíndroma :) ");
+        String palabra = Console.ReadLine();
+        if (esPalindroma(palabra)) {
+           System.out.println("Es palíndroma");
+        } else {
+            System.out.println("NO es palíndroma");
+        }
+
+    static Boolean esPalindroma(String cadena) {
+        Console.WriteLine("Función llamada con => " + cadena);
+        if (cadena.Length < 2) {
+            return true;
+        }
+        Console.WriteLine("Vamos a comparar => " + cadena[0]);
+        Console.WriteLine("Con => " + cadena[cadena.Length - 1]);
+        if (cadena[0] == cadena[cadena.Length - 1]) {
+            return esPalindroma(cadena.Substring(1, cadena.Length - 2));
+        }
+        return false;
+        
+        
+    }
+    }
+
