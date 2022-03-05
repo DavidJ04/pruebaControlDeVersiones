0167e74dff8b88b73fd6324b507e950aa89f61fc
DavidLS <DavidLS@LAPTOP-F803EC2K>
2022-03-04 19:21:44 -0600

    init commit palindromo

--- src/pruebagithub/PruebaGitHub.java
+++ src/pruebagithub/PruebaGitHub.java
@@ -17,27 +17,18 @@ public class PruebaGitHub {
     
     public static void main(String[] args) {
     
+        Scanner teclado = new Scanner(System.in);
+    
         System.out.println("Introduce una palabra y te diré si es palíndroma :) ");
-        String palabra = Console.ReadLine();
+        String palabra = teclado.nextLine();
+        /*
         if (esPalindroma(palabra)) {
            System.out.println("Es palíndroma");
         } else {
             System.out.println("NO es palíndroma");
         }
-
-    static Boolean esPalindroma(String cadena) {
-        Console.WriteLine("Función llamada con => " + cadena);
-        if (cadena.Length < 2 ) {
-            return true;
-        }
-        Console.WriteLine("Vamos a comparar => " + cadena[0]);
-        Console.WriteLine("Con => " + cadena[cadena.Length - 1]);
-        if (cadena[0] == cadena[cadena.Length - 1]) {
-            return esPalindroma(cadena.Substring(1, cadena.Length - 2));
-        }
-        return false;
-        
-        
+        */
+  
     }
     }
 
